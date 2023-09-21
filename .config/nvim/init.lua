7070cc3f8ac552eec06cf8e361c59fde9932b12a
barries <barries1@gmail.com>
2023-09-21 09:12:23 -0400

    lazy.nvim: init
    branch: master

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,6 +21,17 @@ vim.keymap.set({"n", "x"}, "<Leader>m", "<Plug>MarkSet");
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
 vim.opt.rtp:prepend(lazypath);
 
 local plugins = {
